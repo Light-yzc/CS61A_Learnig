62879a7873c55fc63c0652517219368bc70dced8
Light-yzc <3275106634@qq.com>
2024-09-12 21:35:10 +0800

    "Finish hw01"

--- hw01.py
+++ hw01.py
@@ -13,9 +13,9 @@ def a_plus_abs_b(a, b):
     3
     """
     if b < 0:
-        f = _____
+        f = sub
     else:
-        f = _____
+        f = add
     return f(a, b)
 
 def a_plus_abs_b_syntax_check():
@@ -42,7 +42,7 @@ def two_of_three(i, j, k):
     >>> two_of_three(5, 5, 5)
     50
     """
-    return _____
+    return i ** i + j ** j + k ** k - max(i,j,k)
 
 def two_of_three_syntax_check():
     """Check that your two_of_three code consists of nothing but a return statement.
@@ -66,6 +66,11 @@ def largest_factor(n):
     1
     """
     "*** YOUR CODE HERE ***"
+    a = n -1
+    while(a > 0):
+        if n % a == 0:
+            return a
+        a -= 1
 
 
 def hailstone(n):
@@ -88,4 +93,15 @@ def hailstone(n):
     1
     """
     "*** YOUR CODE HERE ***"
+    l = 0
+    print(n)
+    while (n!=1):
+        if n % 2 == 0:
+            n = n / 2
+        else :
+            n = n * 3 + 1
+        print (n)
+        l += 1
+    return l
 
+hailstone(10)
